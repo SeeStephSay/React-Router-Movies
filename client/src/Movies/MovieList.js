2580a586d07a0c09e0281ae345d7b7f626fb7dde
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-12-08 20:14:20 -0600

    Saving changes.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,74 +1,89 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import MovieCard from './MovieCard';
 
-const StyledLink = styled(Link) `
-  text-decoration: none;
-  `;
+// const StyledLink = styled(Link)`
+// 	color: ##ff69b4;
+// 	text-decoration: none;
+//   `;
 
-	export default class MovieList extends Component {
-		constructor(props) {
-			super(props);
-			this.state = {
-				movies: []
-			};
-		}
+export default class MovieList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			movies : [],
+		};
 	}
 
 	componentDidMount() {
 		axios
 			.get('http://localhost:5000/api/movies')
-			.then(response => {
+			.then((response) => {
 				this.setState(() => ({ movies: response.data }));
 			})
-			.catch(error => {
-				console.error('Server Error', error)
+			.catch((error) => {
+				console.error('Server Error', error);
 			});
-		}
+	}
 
+	// const MovieList = (props) => {
+	// 	const [ movies, setMovies ] = useState([]);
+	// 	useEffect(() => {
+	// 		const getMovies = () => {
+	// 			axios
+	// 				.get('http://localhost:5000/api/movies')
+	// 				.then((response) => {
+	// 					setMovies(response.data);
+	// 				})
+	// 				.catch((error) => {
+	// 					console.error('Server Error', error);
+	// 				});
+	// 		};
 
-// const MovieList = (props) => {
-// 	const [ movies, setMovies ] = useState([]);
-// 	useEffect(() => {
-// 		const getMovies = () => {
-// 			axios
-// 				.get('http://localhost:5000/api/movies')
-// 				.then((response) => {
-// 					setMovies(response.data);
-// 				})
-// 				.catch((error) => {
-// 					console.error('Server Error', error);
-// 				});
-// 		};
+	// 		getMovies();
+	// 	}, []);
 
-// 		getMovies();
-// 	}, []);
-
-	return <div className='movie-list'>{movies.map((movie) => <MovieDetails key={movie.id} movie={movie} />)}</div>;
-};
-
-function MovieDetails({ movie }) {
-	const { title, director, metascore, stars } = movie;
-	return (
-		<div className='movie-card'>
-			<h2>{title}</h2>
-			<div className='movie-director'>
-				Director: <em>{director}</em>
-			</div>
-			<div className='movie-metascore'>
-				Metascore: <strong>{metascore}</strong>
-			</div>
-			<h3>Actors</h3>
-
-			{stars.map((star) => (
-				<div key={star} className='movie-star'>
-					{star}
-				</div>
-			))}
-		</div>
-	);
+	render() {
+		return (
+			<div className='movie-list'>{this.state.movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}</div>
+		);
+	}
 }
 
-export default MovieList;
+// 	return <div className='movie-list'>{movies.map((movie) => <MovieDetails key={movie.id} movie={movie} />)}</div>;
+// };
+
+// function MovieDetails({ movie }) {
+// 	const { title, director, metascore, stars } = movie;
+// 	return (
+// 		<div className='movie-card'>
+// 			<h2>{title}</h2>
+// 			<div className='movie-director'>
+// 				Director: <em>{director}</em>
+// 			</div>
+// 			<div className='movie-metascore'>
+// 				Metascore: <strong>{metascore}</strong>
+// 			</div>
+// 			<h3>Actors</h3>
+
+// 			{stars.map((star) => (
+// 				<div key={star} className='movie-star'>
+// 					{star}
+// 				</div>
+// 			))}
+// 		</div>
+// 	);
+// }
+
+// export default MovieList;
+
+// function MovieDetails({ movie }) {
+// 	const { title, director, metascore, stars } = movie;
+// 	return (
+// 		<StyledLink to={`/movies/${movie.id}`}>
+// 			<MovieCard movie={movie} />
+// 		</StyledLink>
+// 	);
+// }
